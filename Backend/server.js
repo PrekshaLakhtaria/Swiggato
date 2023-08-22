46db3cfd7f3a8e1c53d21cc9147700a9c8a06d48
Preksha Lakhtaria <prekshalakhtaria@gmail.com>
2023-08-22 18:10:27 +0530

    added cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const foodData = require("./models/foodModel");
 
+const cors = require("cors");
+
 const db = require("./db.js");
 
 const app = express();
 
 const foodRoute = require("../Backend/routes/foodRoute");
 const userRoute = require("../Backend/routes/userRoute");
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH,PUT,DELETE,OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/foods/", foodRoute);
 app.use("/api/users/", userRoute);
